b59568aa2c645660e0be88afff5bb441f44b156b
Oliver Villarreal <oliverPRG03@gmail.com>
2026-07-30 21:17:47 -0700

    Creación de CRUD en UsuarioService

---
+++ src/main/java/com/ayudamutua/proyecto/service/UsuarioService.java
@@ -0,0 +1,33 @@
+package com.ayudamutua.proyecto.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ayudamutua.proyecto.model.Usuario;
+import com.ayudamutua.proyecto.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	public List<Usuario> regresarAllUsers() {
+		return usuarioRepository.findAll();
+	}
+	public Usuario crearUsuario(Usuario nuevoUsuario) {
+		return usuarioRepository.save(nuevoUsuario);
+	}
+	public Optional<Usuario> buscarUsuarioPorId(Long id) {
+		return usuarioRepository.findById(id);
+	}
+	public void borrarUsuario(Long id) {
+		 usuarioRepository.deleteById(id);
+	}
+	public Usuario actualizarUsuario(Usuario actualizadoUsuario) {
+		return usuarioRepository.save(actualizadoUsuario);
+	}
+}
